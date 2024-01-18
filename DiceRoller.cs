897efed3abc1e180345ff967dcd93b13c0cf8c28
justinchew <justinchew@JUSTINCHEW4493>
2024-01-17 19:12:37 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DiceRollingSimulator
+{
+    public class DiceRoller
+    {
+        private Random random = new Random();
+
+        public int[] RollDice(int numberOfRolls) // Number received from first class
+        {
+            int[] count = new int[11]; // Array to store roll totals (2-12)
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int rollOne = random.Next(1, 7); // Counts from 1-6
+                int rollTwo = random.Next(1, 7);
+                int sum = rollOne + rollTwo;
+                count[sum - 2]++; // Keeps track of how many times a number is rolled
+            }
+
+            return count; // returns count back to first class
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DiceRollingSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+
+            Console.Write("How many dice rolls would you like to simulate? ");
+            int numberOfRolls = int.Parse(Console.ReadLine()); // Converts input to integer and stores it
+
+            DiceRoller roller = new DiceRoller();
+            int[] results = roller.RollDice(numberOfRolls); // Passes number of rolls wanted to second class, and returns results array back to first class
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numberOfRolls + ".");
+
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write(i + ": "); // Prints labels for each roll total (2-12)
+                int percentage = (int)((double)results[i - 2] / numberOfRolls * 100); // Calculates a percentage for each roll total
+                Console.WriteLine(new String('*', percentage)); // Prints a '*' next the label for each percent
+            }
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
